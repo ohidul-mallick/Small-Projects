0d722535e73671b651c1028f26899608adb44388
ohidul-mallick <ohidulmallick999@gmail.com>
2021-06-30 22:08:28 +0530

    Window Created

---
+++ converter/Converter.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+root.title("Docx To Pdf Converter") #Changing Background Color
+root.configure(width=600,height=500,background="#94c42b") # Window Customize
+root.resizable(width=False,height=False)
+
+root.mainloop()
